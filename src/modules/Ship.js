bb078034d98cf5571e323e5291cfe4581777b7fe
JoseD971 <157433709+JoseD971@users.noreply.github.com>
2025-04-19 17:57:25 -0500

    feat: add new method assignName for defining ship names based on its length

--- src/modules/Ship.js
+++ src/modules/Ship.js
@@ -1,7 +1,26 @@
 class Ship {
+    static length3Count = 0;
+
     constructor(length) {
         this.length = length;
         this.hits = 0;
+        this.name = this.assignName();
+    }
+
+    assignName() {
+        switch (this.length) {
+            case 5:
+                return 'Aircraft Carrier';
+            case 4:
+                return 'Battleship';
+            case 3:
+                Ship.length3Count += 1;
+                return Ship.length3Count === 1 ? 'Submarine' : 'Destroyer';
+            case 2:
+                return 'Patrol Boat';
+            default:
+                return 'Unknown Ship';
+        }
     }
 
     hit() {
@@ -9,7 +28,7 @@ class Ship {
     }
 
     isSunk() {
-        return this.hits >= this.length;
+        return this.hits === this.length;
     }
 }
 
